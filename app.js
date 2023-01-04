4ebb5a002cc1c8932a215c7a101ff4ac791b002c
DigiPolitics <steve@campaigning.digital>
2023-01-04 15:52:55 +0000

    FM Assessment Quiz V1

---
+++ app.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+      });
---
+++ script.js
@@ -0,0 +1,27 @@
+var fname;
+
+$(document).ready(function () {
+  $("#page2").hide(0);  
+  $("#page1").show(500);
+    $("#main").addClass("bg-secondary");
+    $("#main").removeClass("bg-primary");
+    $("#btnPage2").click(function () {
+      fname = document.getElementById('firstname').value;
+      if (fname) {
+        $("#page1").hide(500);
+      $('.name-insert').each(function() {
+        $(this).html(fname);
+        if ($(this).is('input')) {
+          $(this).val(fname);
+        }
+      });
+      $("#page2").show(0);
+    } else {
+      alert("Please enter your name"); 
+    }
+    });
+    $("#btnPage1").click(function () {
+      $("#page1").show(500);
+      $("#page2").hide(0);
+    });
+  });
