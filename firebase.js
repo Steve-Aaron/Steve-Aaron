bf0add07a7655d536d7170aa033fb266186f45a2
DigiPolitics <steve@campaigning.digital>
2023-01-12 12:05:08 +0000

    Added space before small

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+
 const firebaseConfig = {
     apiKey: "AIzaSyDGIUHqIYcMWJru32k7Hd_gt3hfi5-42Hg",
     authDomain: "demonstration-project-369716.firebaseapp.com",
@@ -14,45 +15,3 @@ const firebaseConfig = {
 
 // reference your database
 const contactFormDB = firebase.database().ref('contactForm');
-
-document.getElementById('fm-assessment-submission-form').addEventListener('submit', submitForm); // listen for submit event, call submitForm function
-
-function submitForm(e) {
-    e.preventDefault(); // prevent default form submission
-    let firstname = getElementVal("firstname");
-    let lastname = getElementVal("lastname");
-    let email = getElementVal("email_this");
-    let question_one_answer = getElementVal("question-one-answer");
-    let question_two_answer = getElementVal("question-two-answer");
-    let question_three_answer = getElementVal("question-three-answer");
-    let question_four_answer = getElementVal("question-four-answer");
-    let question_five_answer = getElementVal("question-five-answer");
-
-    saveMessage(firstname, lastname, email, question_one_answer, question_two_answer, question_three_answer, question_four_answer, question_five_answer); // save message to firebase
-
-    const successPage = () => {
-        console.log("success_page");
-        $("#page3").show(500);
-        $("#page1").hide(0);
-        $("#page2").hide(0);
-        };
-
-successPage();
-    };
-
-const getElementVal = (id) => document.getElementById(id).value; // get value of input field
-
-const saveMessage = (firstname, lastname, email, question_one_answer, question_two_answer, question_three_answer, question_four_answer, question_five_answer) => {
-    let newContactFormDB = contactFormDB.push();
-    newContactFormDB.set({
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        question_one_answer: question_one_answer,
-        question_two_answer: question_two_answer,
-        question_three_answer: question_three_answer,
-        question_four_answer: question_four_answer,
-        question_five_answer: question_five_answer,
-        date_submitted: Date.now().toString()
-    });
-};
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var fname;
+let fname;
 
 $(document).ready(function () {
   $("#page2").hide(0); $("#page3").hide(0);  
@@ -28,3 +28,50 @@ $(document).ready(function () {
       $("#page3").hide(0);  
     });
   });
+
+  document.getElementById('fm-assessment-submission-form').addEventListener('submit', submitForm); // listen for submit event, call submitForm function
+
+function submitForm(e) {
+    e.preventDefault(); // prevent default form submission
+    let firstname = getElementVal("firstname");
+    let lastname = getElementVal("lastname");
+    let email = getElementVal("email_this");
+    let question_one_question = document.getElementById("question-one-label").innerHTML;
+    let question_one_answer = getElementVal("question-one-answer");
+    let question_two_question = document.getElementById("question-two-label").innerHTML;
+    let question_two_answer = getElementVal("question-two-answer");
+    let question_three_question = document.getElementById("question-three-label").innerHTML;
+    let question_three_answer = getElementVal("question-three-answer");
+    let question_four_question = document.getElementById("question-four-label").innerHTML;
+    let question_four_answer = getElementVal("question-four-answer");
+    let question_five_question = document.getElementById("question-five-label").innerHTML;
+    let question_five_answer = getElementVal("question-five-answer");
+
+    saveMessage(firstname, lastname, email, question_one_answer, question_two_answer, question_three_answer, question_four_answer, question_five_answer); // save message to firebase
+
+    const successPage = () => {
+        console.log("success_page");
+        $("#page3").show(500);
+        $("#page1").hide(0);
+        $("#page2").hide(0);
+        };
+
+successPage();
+    };
+
+const getElementVal = (id) => document.getElementById(id).value; // get value of input field
+
+const saveMessage = (firstname, lastname, email, question_one_answer, question_two_answer, question_three_answer, question_four_answer, question_five_answer) => {
+    let newContactFormDB = contactFormDB.push();
+    newContactFormDB.set({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        question_one_answer: question_one_answer,
+        question_two_answer: question_two_answer,
+        question_three_answer: question_three_answer,
+        question_four_answer: question_four_answer,
+        question_five_answer: question_five_answer,
+        date_submitted: Date.now().toString()
+    });
+};
